abc80f60705136e3a938236f7f3af301fce358a1
Bhavesh Salunke <bhavesh.salunke@massiverocket.com>
2024-03-13 12:00:50 +0530

    Refactor.

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,7 +5,6 @@ import { Box, Flex, Heading, Text, Image, Button, IconButton } from "@chakra-ui/
 import { configs, Content, MarkdownFile, useContent } from "shared/content/Content";
 import { Education } from "pages/about/education/Education";
 import { Experience } from "pages/about/experience/Experience";
-import { Skills } from "pages/about/skills/Skills";
 import { VolumeIcon } from "utils/Icons";
 
 export const About: FC = () => {
